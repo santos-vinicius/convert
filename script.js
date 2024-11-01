ea8cec57ce7c4e746978fac63421e65cf5fd15e6
Vinicius Santos <santosviniv@gmail.com>
2024-11-01 01:25:35 -0300

    [feat] add convert function

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-const amout = document.getElementById("amout")
+// Cotação de moedas do dia
+const USD = 5.25
+const EUR = 5.74
+const GBP = 6.58
 
-// Manipulando o input amout para receber somente números.
-amout.addEventListener("input", () => {
+// Obtendo elementos do formulário
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
-  amout.value = amout.value.replace(hasCharactersRegex, "")
+  amount.value = amount.value.replace(hasCharactersRegex, "")
 } ) 
+
+// Capturanado o evento de submit do formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch(currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+
+}
+
+// Função para converter a moeda. 
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
